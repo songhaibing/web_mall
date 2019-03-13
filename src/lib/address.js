9dedd323e287e5f896a4c6aed3463a741b45d38b
宋海兵 <2269629396@qq.com>
2019-03-13 10:00:01 +0800

    配置项目

---
+++ src/lib/address.js
@@ -0,0 +1,5 @@
+'use strict'
+const IP = 'http://localhost:80/'
+// const IP = 'http://10.200.13.41:8000/'
+
+export default IP
---
+++ src/lib/api.js
@@ -0,0 +1,12 @@
+
+const API = {
+  // login: 'user/login',
+  // enterApplyFor:'enter/enterApplyFor',
+  // applicationStatusQuery:'enter/applicationStatusQuery',
+  // applicationPublic:'enter/applicationPublic',
+  // applicationPublicFacility:'Facility/applicationPublicFacility',
+  // getPublicFacilityList:'Facility/getPublicFacilityList',
+  // SeeTheNews:'WechatNews/SeeTheNews',
+  // logout:'user/logout'
+}
+export default API
