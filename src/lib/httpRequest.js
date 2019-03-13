689d6c5e663fc4914b5c36cc095c0965b98f78a3
wyongjian347 <695648896@qq.com>
2019-03-13 16:03:49 +0800

    启动页

--- src/lib/httpRequest.js
+++ src/lib/httpRequest.js
@@ -1,6 +1,6 @@
-'use strict'
-import axios from 'axios'
-import qs from 'qs'
+'use strict';
+import axios from 'axios';
+import qs from 'qs';
 import { Toast } from 'vant';
 /**
  * 提示函数
@@ -12,25 +12,22 @@ const tip = msg => {
     duration: 1000,
     forbidClick: true
   });
-}
+};
 // 环境的切换
 if (process.env.NODE_ENV == 'development') {
-  axios.defaults.baseURL = '/api';//开发环境
+  axios.defaults.baseURL = '/api'; //开发环境
 } else if (process.env.NODE_ENV == 'debug') {
-  axios.defaults.baseURL = '';//测试环境
+  axios.defaults.baseURL = ''; //测试环境
 } else if (process.env.NODE_ENV == 'production') {
-  axios.defaults.baseURL = 'http://api.123dailu.com/';//生产环境
+  axios.defaults.baseURL = 'http://api.123dailu.com/'; //生产环境
 }
 
 // 请求超时时间
 axios.defaults.timeout = 10000;
 
-
-
-let HTTP = {}
+let HTTP = {};
 // 使用由axios库提供的配置的默认值来创建axios实例
-let instance = axios.create()
-
+let instance = axios.create();
 
 /**
  * 发送post请求
@@ -39,10 +36,11 @@ let instance = axios.create()
  * @param data 传递给后端的数据
  * @param callback 回调函数,参数 res.data.data 为主要数据部分
  */
-HTTP.post = function (url, data, callback) {
-  let params = qs.stringify(data)
-  instance.post(url, params)
-    .then(function (res) {
+HTTP.post = function(url, data, callback) {
+  let params = qs.stringify(data);
+  instance
+    .post(url, params)
+    .then(function(res) {
       //响应成功回调
       if (res.data.status === 10000) {
         callback(res.data.data);
@@ -50,8 +48,8 @@ HTTP.post = function (url, data, callback) {
         // alert(ERROR_CODE[res.data.status]);
       }
     })
-    .catch(function (err) {
-          tip(err)
+    .catch(function(err) {
+      tip(err);
     });
 };
 
@@ -62,20 +60,21 @@ HTTP.post = function (url, data, callback) {
  * @param data 传递给后端的数据
  * @param callback
  */
-HTTP.get = function (url, data, callback) {
-  let params = {params: data}
-  instance.get( url, params)
-    .then(function (res) {
+HTTP.get = function(url, data, callback) {
+  let params = { params: data };
+  instance
+    .get(url, params)
+    .then(function(res) {
       //响应成功回调
       if (res.data.status === 10000) {
-        callback(res.data.data)
+        callback(res.data.data);
       } else {
         // alert(ERROR_CODE[res.data.status]);
       }
     })
-    .catch(function (err) {
-        tip(err)
+    .catch(function(err) {
+      tip(err);
     });
 };
 
-export default HTTP
+export default HTTP;
